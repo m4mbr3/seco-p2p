9ec251bae815f6f95f47c39d5ab745441d580b5a
nguyenho83@gmail.com <nguyenho83@gmail.com@5ce060ae-ef71-ab1f-d58e-59ed3e9fcf6a>
2011-12-07 14:38:05 +0000

    -Add getEngineById and getServiceById in ServiceRepository.java -Add RemoteInvoker.java class -AddServiceValidation.java class

---
+++ secop2p/src/secop2p/RemoteInvoker.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package secop2p;
+
+/**
+ *
+ * @author Nguyen Ho
+ * Description: Building a http request to remote service
+ */
+public class RemoteInvoker {
+    
+}
