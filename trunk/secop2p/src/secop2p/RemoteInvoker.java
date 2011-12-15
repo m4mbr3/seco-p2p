6281328d0474e8f7cd40933c8281b98faab0c688
mambro007@gmail.com <mambro007@gmail.com@5ce060ae-ef71-ab1f-d58e-59ed3e9fcf6a>
2011-12-15 21:50:46 +0000

    Creation Map & start with RemoteInvoker

--- trunk/secop2p/src/secop2p/RemoteInvoker.java
+++ trunk/secop2p/src/secop2p/RemoteInvoker.java
@@ -9,6 +9,13 @@ package secop2p;
  * @author Nguyen Ho
  * Description: Building a http request to remote service
  */
-public class RemoteInvoker {
-    
+public class RemoteInvoker implements HttpInvoker {
+
+             /** Logger. */
+            private static final Logger LOG = LoggerFactory.getLogger(RemoteInvoker.class);
+            ServiceRepository sr = new ServiceRepository();
+            public RemoteInvoker ()
+            {
+
+            }
 }
