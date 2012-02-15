adbde89b22b1706a969268341f97fdb13e79f5af
eros.lever@gmail.com <eros.lever@gmail.com@5ce060ae-ef71-ab1f-d58e-59ed3e9fcf6a>
2012-02-15 11:03:51 +0000

    Modified AliveEngine into EngineMonitor

--- secop2p/src/secop2p/EngineMonitor.java
+++ secop2p/src/secop2p/EngineMonitor.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.logging.Level;
@@ -23,17 +24,19 @@ import secop2p.util.Serializer;
  *
  * @author eros
  */
-public class AliveEngine implements ListenerCallback, Sender.MessageGenerator, Sender.TargetGenerator {
+public class EngineMonitor implements ListenerCallback, Sender.MessageGenerator, Sender.TargetGenerator {
 
     private final EngineInfo engineInfo;
     private final ServiceRepositoryProxy srp;
+    private Map<EngineInfo, Message> messages;
 
-    public AliveEngine(EngineInfo ei) throws IOException{
+
+    public EngineMonitor(EngineInfo ei) throws IOException{
         this(ei, new ServiceRepositoryProxy(ei));
     }
 
     @SuppressWarnings("ResultOfObjectAllocationIgnored")
-    public AliveEngine(EngineInfo ei, ServiceRepositoryProxy srp){
+    public EngineMonitor(EngineInfo ei, ServiceRepositoryProxy srp){
         engineInfo = ei;
         this.srp = srp;
         ServerSocketChannel ssc = PortChecker.getBoundedServerSocketChannelOrNull(ei.getAlivePort());
@@ -48,10 +51,10 @@ public class AliveEngine implements ListenerCallback, Sender.MessageGenerator, S
             byte[] msg = Serializer.serialize(new Message(this.engineInfo, m));
             return msg;
         }catch(IOException ex){
-            Logger.getLogger(AliveEngine.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(EngineMonitor.class.getName()).log(Level.SEVERE, null, ex);
             return null;
         }catch(Exception ex){
-            Logger.getLogger(AliveEngine.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(EngineMonitor.class.getName()).log(Level.SEVERE, null, ex);
             return null;
         }
     }
@@ -61,11 +64,11 @@ public class AliveEngine implements ListenerCallback, Sender.MessageGenerator, S
             Message m;
             try {
                 m = Serializer.deserialize(client.socket().getInputStream(), Message.class);
-                System.out.println(m);
+                messages.put(m.from, m);
             } catch (IOException ex) {
-                Logger.getLogger(AliveEngine.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(EngineMonitor.class.getName()).log(Level.SEVERE, null, ex);
             } catch (ClassNotFoundException ex) {
-                Logger.getLogger(AliveEngine.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(EngineMonitor.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
@@ -75,12 +78,17 @@ public class AliveEngine implements ListenerCallback, Sender.MessageGenerator, S
         try {
             for (EngineInfo ei : srp.getEngines()) {
                 if( ei != this.engineInfo)
-                    targets.add(ei.getSocketAddress());
+                    targets.add(ei.getAliveSocketAddress());
             }
         } catch (IOException ex) {
-            Logger.getLogger(AliveEngine.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(EngineMonitor.class.getName()).log(Level.SEVERE, null, ex);
         }
         return targets;
+    }
+
+    public Set<EngineInfo> getAliveEngines(){
+        //TODO
+        return null;
     }
 
 }
