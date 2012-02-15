4ddc00b72f0da805aa88afb0b656e78dcfa030a7
eros.lever@gmail.com <eros.lever@gmail.com@5ce060ae-ef71-ab1f-d58e-59ed3e9fcf6a>
2012-02-15 10:01:46 +0000

    Oops... (I did it again :P) forgot adding one class

---
+++ trunk/secop2p/src/secop2p/AliveMessage.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package secop2p;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author eros
+ */
+public class AliveMessage implements Serializable {
+
+    private final EngineInfo from;
+    private final long timestamp;
+    private final Metrics metrics;
+
+    public AliveMessage(final EngineInfo ei, final Metrics m){
+        from = ei;
+        timestamp = System.currentTimeMillis();
+        metrics = m;
+    }
+
+    public EngineInfo getFrom() {
+        return from;
+    }
+
+    public Metrics getMetrics() {
+        return metrics;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+}
